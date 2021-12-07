2014903b96d5663546dc749a26fbf2b6a7d52190
Vivek Kumar <developer.vivekkumar@gmail.com>
2021-12-07 11:36:35 +0530

    added google verification meta tag

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import Home from './screens/home/Home';
 import { Route, Routes } from 'react-router-dom';
 import Catalogue from './screens/catalogue/Catalogue';
 import ProductDetails from './screens/details/ProductDetails';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return <Routes>
+    <Helmet>
+      <meta name="google-site-verification" content="_9K0rqB008VayXRnOX0UM5JRBw6forTqpRguAxT0tUI" />
+    </Helmet>
     <Route path="/" element={<Home />} />
     <Route path="/catalogue" element={<Catalogue />} />
     <Route path="/catalogue/:slug" element={<ProductDetails />} />
