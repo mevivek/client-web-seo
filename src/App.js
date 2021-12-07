1d0e7f5db54b0c9889b276070feae1c8eb2cc389
Vivek Kumar <developer.vivekkumar@gmail.com>
2021-12-07 12:06:00 +0530

    removed helmet import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './screens/home/Home';
 import { Route, Routes } from 'react-router-dom';
 import Catalogue from './screens/catalogue/Catalogue';
 import ProductDetails from './screens/details/ProductDetails';
-import { Helmet } from 'react-helmet';
 
 function App() {
   return <Routes>
